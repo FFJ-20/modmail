ae584b564a18e905dbf42cd64c0b9130f530299c
FFJ-20 <80383582+FFJ-20@users.noreply.github.com>
2021-03-10 13:00:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js")
+const client = new discord.Client()
+const config = require("./config.json")
+const fs = require("fs")
+
+client.commands = new discord.Collection()
+
+let files = fs.readdirSync("./commands/").filter(f => f.endsWith(".js"))
+
+for (let commands of files) {
+  
+  let cmd = require("./commands/" + commands)
+  
+  if (cmd.name) {
+    client.commands.set(cmd.name, cmd)
+  }
+  
+}
+
+
+client.on("ready", async () => {
+  require("./events/ready.js")(client)
+})
+
+client.on("message", async message => {
+  require("./events/message.js")(message, client)
+})
+
+client.login(config.token)

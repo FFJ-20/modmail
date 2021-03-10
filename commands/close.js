87d6ee1dd5ce60e3579e39d981726a218da2aed7
FFJ-20 <80383582+FFJ-20@users.noreply.github.com>
2021-03-10 13:10:47 +0530

    Create close.js

---
+++ commands/close.js
@@ -0,0 +1,26 @@
+const db = require("quick.db")
+const config = require("../config.json")
+
+module.exports = {
+  name: "close",
+  description: "Closes a Ticket",
+  run: async (message, args, client) => {
+    
+    let data = db.get(`ticket_${message.channel.id}`)
+    
+    if (data === null) return message.channel.send(":x: This is not a Ticket Channel.")
+    
+    if (!config.roles) return console.log("No Roles has been Added in Config.")
+    
+    if (!Array.isArray(config.roles)) return console.log("Roles is not an Array in Config.")
+    
+    if (!message.member.roles.cache.some(r => config.roles.includes(r.id))) return message.channel.send(":x: No Permission!")
+   
+    let user = client.users.cache.get(data)
+    
+    let channel = message.channel
+    
+    require("../execute/close.js")(user, channel)
+    
+  }
+}

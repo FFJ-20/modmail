580ebbe412f5ead9972adcaf6acbb4bd2a0fe10f
FFJ-20 <80383582+FFJ-20@users.noreply.github.com>
2021-03-12 11:29:21 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,15 +4,15 @@ module.exports = async (client) => {
   
   console.log(client.user.tag + " is Online\nMade by Fighter for Justice 2.0")
   
-  if (!config.activity) config.activity = "DMs."
-  
-  if (!config.type) config.type = "WATCHING"
-  
-  if (!config.status) config.status = "online"
-  
+  if (config.activity === "null") config.activity = null
+
+  if (config.type === "null") config.type = null
+
+  if (config.status === "null") config.status = null
+
   client.user.setPresence({
     activity: {
-      "name": config.activity,
+      "name": config.activity
       "type": config.type
     },
     status: config.status

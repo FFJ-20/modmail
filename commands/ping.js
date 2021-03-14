9cb07fbbbc2eba7b75fe15c0bf7696036643fd46
FFJ-20 <80383582+FFJ-20@users.noreply.github.com>
2021-03-14 16:48:11 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports = {
   name: "ping",
-  description: "Shows Bot's Ping",
+  aliases: [],
   run: async (message, args, client) => {
     
     message.channel.send(":ping_pong: Pinging...").then(m => m.edit(`:ping_pong: Pong!\nBot Ping - ${client.ws.ping}ms\nAPI Ping - ${Date.now() - m.createdTimestamp}ms`))

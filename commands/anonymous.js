825a709e1fe4ad02ddbaff09fb6a7e4bfd01c440
FFJ-20 <80383582+FFJ-20@users.noreply.github.com>
2021-03-17 11:59:48 +0530

    Update anonymous.js

--- commands/anonymous.js
+++ commands/anonymous.js
@@ -29,7 +29,7 @@ module.exports = {
    
    let anonymous = true
     
-    require("../execute/senddm.js")(message, user, channel, anonymous, client)
+    require("../execute/senddm.js")(message, args, user, channel, anonymous, client)
     
   }
 }

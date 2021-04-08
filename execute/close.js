ef7d04c57c47072fe9ae9dea4775f093a8196c41
FFJ-20 <80383582+FFJ-20@users.noreply.github.com>
2021-04-08 22:20:02 +0530

    Update close.js

--- execute/close.js
+++ execute/close.js
@@ -1,3 +1,4 @@
+const discord = require("discord.js")
 const db = require("quick.db")
 
 module.exports = async (user, channel) => {
@@ -6,8 +7,13 @@ module.exports = async (user, channel) => {
   
   let channeldata = db.get(`ticket_${channel.id}`)
   
+  let embed = new discord.MessageEmbed()
+  .setTitle("Ticket Closed")
+  .setColor("RANDOM")
+  .setDescription("Your Ticket has been Closed.\nResponding now will create a new ticket.\n\nFeel free to contact us again if you need any kind of help.\nThank you for using our ModMail Service.")
+  
   if (userdata !== null) {
-    user.send("Your Ticket has been closed.\nResponding will make a New Ticket.").catch(() => {})
+    user.send(embed).catch(() => {})
     db.delete(`ticket_${user.id}`)
   }
   
